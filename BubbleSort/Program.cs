e6b7983cd3628853f4ba440c7b0678b89da4cced
Vladyslav Zotov <vladislavzotov94@gmail.com>
2024-01-25 13:01:14 +0100

    add BubbleSort issue

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace BubbleSort
+﻿using BubbleSort;
+
+int[] array = new int[] { 8, 5, 2, 9, 5, 6, 3, 2 };
+new BubbleSortIssue().BubbleSort(array);
+
+foreach (var item in array)
+{
+    Console.WriteLine(item);
+}
+
+namespace BubbleSort
 {
     public class BubbleSortIssue
     {
-        public static int[] BubbleSort(int[] array)
+        public int[] BubbleSort(int[] array)
         {
-            // Write your code here.
+            bool isSorted = false;
+            int iteration = 0;
+
+            while (!isSorted)
+            {
+                isSorted = true;
+                for (int i = 0; i < array.Length - 1 - iteration; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        isSorted = false;
+                    }
+                }
+                iteration++;
+            }
+
             return array;
         }
     }
