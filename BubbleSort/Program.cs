581c1d561fb04456293e617499a589406f8d2b67
Vladyslav Zotov <vladislavzotov94@gmail.com>
2024-01-25 11:56:30 +0100

    add solution and first project

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BubbleSort
+{
+    public class BubbleSortIssue
+    {
+        public static int[] BubbleSort(int[] array)
+        {
+            // Write your code here.
+            return array;
+        }
+    }
+}
